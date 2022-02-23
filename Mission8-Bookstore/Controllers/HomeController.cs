27bc69537171fa115bff1061fa7ca4f965c89414
Matthew Mazankowski <m.g.mazankowski@gmail.com>
2022-02-23 16:30:13 -0700

    copied over mission 7

---
+++ Mission8-Bookstore/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission7_Bookstore.Models;
+using Mission7_Bookstore.Models.ViewModels;
+
+namespace Mission7_Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private iBookstoreRepository repo;
+
+        public HomeController(iBookstoreRepository temp)
+        {
+            repo = temp; 
+        }
+
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+
+        //public HomeController(BookstoreContext temp)
+        //{
+        //    context = temp;
+        //}
+
+        //private BookstoreContext context { get; set; }
+
+        //public HomeController(BookstoreContext temp) => context = temp; 
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ Mission8-Bookstore/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Mission7_Bookstore.Models.ViewModels;
+
+namespace Mission7_Bookstore.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-blah")]
+    public class PaginationTagHelper : TagHelper 
+    {
+        //Dynamically create the page links for us
+        private IUrlHelperFactory uhf;
+
+        public PaginationTagHelper (IUrlHelperFactory temp)
+        {
+            uhf = temp; 
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext vc { get; set; }
+
+        //Different then the view context (the attributes above on apply to viewcontext)
+        public PageInfo PageBlah { get; set; }
+
+        public string PageAction { get; set; }
+
+        public override void Process (TagHelperContext thc, TagHelperOutput tho)
+        {
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i <= PageBlah.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+
+                tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb); 
+            }
+
+            tho.Content.AppendHtml(final.InnerHtml);
+        }
+    }
+}
